1249f25dfd14e622cd23d7f5aed3afc28a20dbd9
kalavalashivaharikowshik <kalavalakowshik1616@gmail.com>
2026-03-16 00:15:41 +0530

    Update MySQL connection to use environment variables
    Refactored MySQL connection to use environment variables for configuration.

--- database/mysql_connector.py
+++ database/mysql_connector.py
@@ -1,51 +1,26 @@
 import mysql.connector
-from mysql.connector import Error
-
+import os
 
 class MySQLConnector:
 
     def __init__(self):
-        self.connection = None
 
-    def connect(self):
-        try:
-            self.connection = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="kowshik@123",
-                database="agentic_supply_chain"
-            )
-
-            if self.connection.is_connected():
-                print("Connected to MySQL Database")
-
-        except Error as e:
-            print("Error while connecting to MySQL:", e)
-
-    def get_connection(self):
-        return self.connection
-
-    def execute_query(self, query, params=None):
-        cursor = self.connection.cursor()
-        cursor.execute(query, params if params else ())
-        self.connection.commit()
-        cursor.close()
+        self.connection = mysql.connector.connect(
+            host=os.getenv("MYSQLHOST"),
+            user=os.getenv("MYSQLUSER"),
+            password=os.getenv("MYSQLPASSWORD"),
+            database=os.getenv("MYSQLDATABASE"),
+            port=os.getenv("MYSQLPORT")
+        )
 
     def fetch_all(self, query):
+
         cursor = self.connection.cursor(dictionary=True)
+
         cursor.execute(query)
+
         result = cursor.fetchall()
-        cursor.close()
-        return result
 
-    def fetch_one(self, query):
-        cursor = self.connection.cursor(dictionary=True)
-        cursor.execute(query)
-        result = cursor.fetchone()
         cursor.close()
-        return result
 
-    def close(self):
-        if self.connection.is_connected():
-            self.connection.close()
-            print("MySQL connection closed")
+        return result
